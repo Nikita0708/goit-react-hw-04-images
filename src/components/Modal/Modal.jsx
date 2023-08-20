59d5405c32b89c2c0dbea6e1652da771f5312505
Nikita0708 <91466168+Nikita0708@users.noreply.github.com>
2023-08-21 01:00:52 +0200

    ok

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { useState, useEffect } from 'react';
+import React from 'react';
+import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import style from './Modal.module.css';
 
